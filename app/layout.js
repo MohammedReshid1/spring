9070a37df0de7d08eda432674ac21d7534175f1c
MohammedReshid1 <93836271+MohammedReshid1@users.noreply.github.com>
2025-01-10 11:28:13 +0300

    favicon

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${montserrat.variable} font-sans`}>
       <title>Spring of Knowledge Academy</title>
+      <link rel="icon" type="image/x-icon" href="./favicon.ico"></link>
       <body className={montserrat.className}>
         <Header />
         <main>{children}</main>

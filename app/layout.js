77e77a17c9d833626a77de1015901d90aaf98422
MohammedReshid1 <93836271+MohammedReshid1@users.noreply.github.com>
2025-01-10 11:16:50 +0300

    Title

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${montserrat.variable} font-sans`}>
+      <title>Spring of Knowledge Academy</title>
       <body className={montserrat.className}>
         <Header />
         <main>{children}</main>

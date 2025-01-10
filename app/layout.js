40abae3a2d2ea7c75f09dad6faac44c2dbc3b202
MohammedReshid1 <93836271+MohammedReshid1@users.noreply.github.com>
2025-01-10 11:50:22 +0300

    helo

--- app/layout.js
+++ app/layout.js
@@ -5,21 +5,31 @@ import Footer from './components/Footer';
 
 export const metadata = {
   title: 'Spring of Knowledge Academy',
-  description: 'Empowering minds through education',
+  description: 'Where Excellence is a Habit ! Not a Goal',
   openGraph: {
     title: 'Spring of Knowledge Academy',
-    description: 'Empowering minds through education',
-    url: 'https://www.springofknowledge.org', // Replace with your actual URL
+    description: 'Where Excellence is a Habit ! Not a Goal',
+    url: 'https://Springofknowledge.org',
     siteName: 'Spring of Knowledge Academy',
     images: [
       {
-        url: 'https://www.springofknowledge.org/images/Spring_Icon.png', // Replace with your actual image URL
-        width: 1200,
-        height: 630,
+        url: 'https://opengraph.b-cdn.net/production/images/0112b273-e231-4d4d-b90d-7f3ddf302f82.png?token=b9x6WIE0-Ssqg4WD64pcaFjB_0-rmVB16c1yDKhIbkk&height=500&width=500&expires=33272498923',
+        width: 500,
+        height: 500,
       },
     ],
     locale: 'en_US',
     type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    domain: 'Springofknowledge.org',
+    url: 'https://Springofknowledge.org',
+    title: 'Spring of Knowledge Academy',
+    description: 'Where Excellence is a Habit ! Not a Goal',
+    images: [
+      'https://opengraph.b-cdn.net/production/images/0112b273-e231-4d4d-b90d-7f3ddf302f82.png?token=b9x6WIE0-Ssqg4WD64pcaFjB_0-rmVB16c1yDKhIbkk&height=500&width=500&expires=33272498923'
+    ],
   },
   icons: {
     icon: '/favicon.ico',
